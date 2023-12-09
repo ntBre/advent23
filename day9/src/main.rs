71080bfd5dbcc28f8d705954af592fdde4d97702
Brent Westbrook <brentrwestbrook@gmail.com>
2023-12-09 10:32:06 -0500

    part2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,21 +2,13 @@ use std::fs::read_to_string;
 
 fn main() {
     let inp = std::env::args().nth(1).unwrap();
-    let sum = part1(&inp);
+    let sum = part2(&inp);
     dbg!(sum);
 }
 
+#[allow(unused)]
 fn part1(inp: &str) -> isize {
-    let s = read_to_string(inp).unwrap();
-
-    let mut lines = Vec::new();
-    for line in s.lines() {
-        let l: Vec<_> = line
-            .split_ascii_whitespace()
-            .map(|s| s.parse::<isize>().unwrap())
-            .collect();
-        lines.push(l);
-    }
+    let lines = load(inp);
 
     let mut sum = 0;
     for line in lines {
@@ -31,6 +23,38 @@ fn part1(inp: &str) -> isize {
         sum += rows[0].last().unwrap();
     }
     sum
+}
+
+fn part2(inp: &str) -> isize {
+    let lines = load(inp);
+
+    let mut sum = 0;
+    for line in lines {
+        let mut rows = reduce(line);
+        let mut a;
+        for r in (1..rows.len()).rev() {
+            let x = rows[r].first().unwrap();
+            let y = rows[r - 1].first().unwrap();
+            a = y - x;
+            rows[r - 1].insert(0, a);
+        }
+        sum += rows[0].first().unwrap();
+    }
+    sum
+}
+
+fn load(inp: &str) -> Vec<Vec<isize>> {
+    let s = read_to_string(inp).unwrap();
+
+    let mut lines = Vec::new();
+    for line in s.lines() {
+        let l: Vec<_> = line
+            .split_ascii_whitespace()
+            .map(|s| s.parse::<isize>().unwrap())
+            .collect();
+        lines.push(l);
+    }
+    lines
 }
 
 fn reduce(mut line: Vec<isize>) -> Vec<Vec<isize>> {
@@ -55,5 +79,10 @@ mod tests {
     #[test]
     fn p1() {
         assert_eq!(part1("input"), 1479011877);
+    }
+
+    #[test]
+    fn p2() {
+        assert_eq!(part2("input"), 973);
     }
 }
